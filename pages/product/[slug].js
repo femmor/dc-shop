e3692495c5962272d34fe387878a65aa9a56ba19
Emmanuel Egomson <fegomson@gmail.com>
2022-04-25 17:25:31 -0500

    Implements similar products

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -6,6 +6,7 @@ import {
   AiFillStar,
   AiOutlineStar,
 } from "react-icons/ai";
+import Product from "../../components/Product";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
@@ -62,6 +63,17 @@ const ProductDetails = ({ product, products }) => {
             <button type="button" className="buy-now" onClick="">
               Buy Now
             </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>Similar products you may like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map(item => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>

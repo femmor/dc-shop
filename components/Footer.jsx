f8d449b58ffa1102a9da1bfa0145d5b980c5e01e
Emmanuel Egomson <fegomson@gmail.com>
2022-04-25 16:43:35 -0500

    Implements the footer and add scrollbar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
+  const year = new Date().getFullYear()
+
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>&copy; {year} DC-Shop Accessories. All Rights Reserved.</p>
+      <p className="icons">
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
+      </p>
+    </div>
   )
 }
 
